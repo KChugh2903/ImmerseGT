128b3f48be19cdb6512f72547f392211dd4aa9a1
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-04-08 17:27:47 -0400

    added script color for background board

---
+++ Unity AR/Assets/ExampleAssets/Scripts/ChangeAlbedoColor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAlbedoColor : MonoBehaviour
+{
+    public Color lightColor;
+    public Color darkColor;
+
+    new private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        ApplyHalfHalfColor();
+    }
+
+    void Update(){
+        ApplyHalfHalfColor();
+    }
+
+    private void ApplyHalfHalfColor()
+    {
+        Material material = renderer.material;
+
+        material.SetColor("_Color", lightColor);
+        material.SetFloat("_Glossiness", 0.6f);
+
+        material.SetColor("_Color2", darkColor);
+        material.SetFloat("_Glossiness2", 0.6f);
+
+        // Set the blend mode to mix the colors evenly
+        material.SetFloat("_Blend", 0.5f);
+    }
+
+
+}
