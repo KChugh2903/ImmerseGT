554d527d8ed3f17fac40645a691586295659112b
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-04-08 15:41:28 -0400

    added pointlight for 3 bridges

--- Unity AR/Assets/BridgeFunc.cs
+++ Unity AR/Assets/BridgeFunc.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class BridgeFunc : MonoBehaviour
 {
-    public Light pointLight;
+    public Light pointLight1;
+    public Light pointLight2;
+    public Light pointLight3;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject taggedPointLight = GameObject.FindWithTag("PointLight3");
-        // Debug.Log("Hello" + tagged);
+        GameObject gameObject = new GameObject();
+        GameObject taggedPointLight1 = GameObject.FindWithTag("PointLight1");
+        GameObject taggedPointLight2 = GameObject.FindWithTag("PointLight2");
+        GameObject taggedPointLight3 = GameObject.FindWithTag("PointLight3");
 
-        if (taggedPointLight != null){
-            pointLight = taggedPointLight.GetComponent<Light>();
-        } else{
-            Debug.LogError("Could not find game object with tag 'PointLight3'");
-        }
-        
+
+        // Debug.Log("Hello" + tagged);
+        pointLight1 = taggedPointLight1.GetComponent<Light>();
+        pointLight2 = taggedPointLight2.GetComponent<Light>();
+        pointLight3 = taggedPointLight3.GetComponent<Light>();
     }
 
     // Update is called once per frame
@@ -27,10 +30,20 @@ public class BridgeFunc : MonoBehaviour
 
     void OnMouseDown()
     {
-        // Turn on the point light
-        pointLight.enabled = true;
-        
-        // Wait for 3 seconds before turning off the point light
-        // StartCoroutine(TurnOffLight(0.5f));
+        if (gameObject.name == "Bridge1") {
+            pointLight1.enabled = true;
+            pointLight2.enabled = false;
+            pointLight3.enabled = false;
+        }
+        else if (gameObject.name == "Bridge2") {
+            pointLight1.enabled = false;
+            pointLight2.enabled = true;
+            pointLight3.enabled = false;
+        }
+        else if (gameObject.name == "Bridge3") {
+            pointLight1.enabled = false;
+            pointLight2.enabled = false;
+            pointLight3.enabled = true;
+        }
     }
 }
