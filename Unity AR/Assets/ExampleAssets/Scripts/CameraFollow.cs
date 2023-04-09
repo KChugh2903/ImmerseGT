9c36a9b6ddebbff17da13791ef8c2b6c5bcf1db9
Kanav Chugh <kanavchugh@lawn-128-61-33-229.lawn.gatech.edu>
2023-04-08 23:15:58 -0400

    New Commit

---
+++ Unity AR/Assets/ExampleAssets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class CameraFollow : MonoBehaviour {
+     public Transform target;
+     public float smoothTime = 0.3f;
+     private Vector3 velocity = Vector3.zero;
+
+     private void FixedUpdate() {
+          if (target == null) { return; }
+          Vector3 targetPosition =
+                 target.position + new Vector3(0, 0, -1);
+          transform.position = Vector3.SmoothDamp(
+                 transform.position,
+                 targetPosition,
+                 ref velocity,
+                 smoothTime);
+     }
+}
